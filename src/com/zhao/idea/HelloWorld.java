b601401918a3445900868a619903e906bdb4cb94
rookieisme111 <1183914394@qq.com>
2022-09-19 22:54:22 +0800

    新的修改

--- src/com/zhao/idea/HelloWorld.java
+++ src/com/zhao/idea/HelloWorld.java
@@ -16,14 +16,20 @@ import java.util.Date;
  * 10.错误的快速定位 F2 或者 shift+F2
  * 11.查看近几次的复制内容并选择粘贴 CTRL+shift+V
  * 12.方法的调用层次 alt+F7 或者 ctrl+alt+h
+ * 13.调试时，查看已有变量的值 alt+F8
+ * 14，调试的快捷键 F8 SHIFT+F8 F7 CTRL+ALT+F7
  */
 public class HelloWorld {
     public static void main(String[] args) {
 
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
         // 自动补全类型及变量 快捷键 ctrl+alt+v 或者 alt+enter
         String zhao = "zhao";
         Date date = new Date();
-        String s = zhao.substring(1, 10);
+
+        String s = zhao.substring(1, zhao.length());
         // 将要输出的内容后面加 .sout 或者 直接输入sout也可以生成完整的输出方法，不过要把内容填进参数
 
         System.out.println(zhao);
